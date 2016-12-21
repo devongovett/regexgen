90606e316d4b95f2c0cb95eef2b1e8a49cce34bc
Devon Govett <devongovett@gmail.com>
2016-12-20 21:53:52 -0500

    Add tests

---
+++ test/test.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+const regexgen = require('../');
+
+describe('regexgen', function () {
+  it('should generate a char class', function () {
+    assert.deepEqual(regexgen(['a', 'b', 'c']), /[a-c]/);
+  });
+
+  it('should generate an alternation', function () {
+    assert.deepEqual(regexgen(['abc', '123']), /123|abc/);
+  });
+
+  it('should extract common prefixes at the start', function () {
+    assert.deepEqual(regexgen(['foobar', 'foozap']), /foo(?:zap|bar)/);
+  });
+
+  it('should extract common prefixes at the end', function () {
+    assert.deepEqual(regexgen(['barfoo', 'zapfoo']), /(?:zap|bar)foo/);
+  });
+
+  it('should extract common prefixes at the start and end', function () {
+    assert.deepEqual(regexgen(['foobarfoo', 'foozapfoo']), /foo(?:zap|bar)foo/);
+  });
+
+  it('should generate an optional group', function () {
+    assert.deepEqual(regexgen(['foo', 'foobar']), /foo(?:bar)?/);
+  });
+
+  it('should generate multiple optional groups', function () {
+    assert.deepEqual(regexgen(['f', 'fo', 'fox']), /f(?:ox?)?/);
+  });
+
+  it('should escape meta characters', function () {
+    assert.deepEqual(regexgen(['foo|bar[test]+']), /foo\|bar\[test\]\+/);
+  });
+
+  it('should escape non-ascii characters', function () {
+    assert.deepEqual(regexgen(['🎉']), /\uD83C\uDF89/);
+  })
+});
